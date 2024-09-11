e42353afb0caeadbbd0282312963b975ac002e1a
AnahiMamani <narielanahi@gmail.com>
2024-09-11 20:17:35 -0300

    reposicionando paginas no handlebars

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -8,6 +8,9 @@ router.get("/", Controller.renderIndex);
 // Rota para renderizar a página de login
 router.get("/login", Controller.renderLogin);
 
+// Rota para renderizar a página de cadastro
+router.get("/cadastro", Controller.renderCadastro);
+
 // Metodo do Controler para retornar uma resposta para o cliente
 router.post("/cadastro", Controller.cadastrarUsuario);
 
